d996b840f0d101b79ecf97efd0cb254049d0dad7
codingisacopingstrategy <eric@ericschrijver.nl>
2011-09-13 14:51:55 +0200

    All the tags
    in a python script

---
+++ tags.py
@@ -0,0 +1,140 @@
+-------------------------------------------------
+|                                               |
+|   blabloblab      blabloblab      blabloblab  |
+|   berjbklb        berjbklb        berjbklb    |
+|   fjjblkrbl       fjjblkrbl       fjjblkrbl   |
+|                                               |
+|   blabloblab      blabloblab      blabloblab  |
+|   berjbklb        berjbklb        berjbklb    |
+|   fjjblkrbl       fjjblkrbl       fjjblkrbl   |
+|                                               |
+|   blabloblab      blabloblab      blabloblab  |
+|   berjbklb        berjbklb        berjbklb    |
+|   fjjblkrbl       fjjblkrbl       fjjblkrbl   |
+|                                               |
+-------------------------------------------------
+
+
+
+do a with
+bcdefgh
+ab ac ad ae af ag ah
+do b with
+cdefgh
+bc bd be bg bg bh
+do c with
+efgh
+ce cf cg ch
+do e with
+fgh
+ef eg eh
+do f with
+gh
+fg fh
+do g with
+h
+gh
+
+
+01 02 03 04 05 06
+12 13 14 15 16
+23 24 25 26
+
+
+for i in list:
+    for i in range()
+
+
+for y in range(len(lijst)-1):
+    for x in range(y,len(lijst)-1):
+        print ascii_lowercase[y], ascii_lowercase[x+1]
+        
+def combine(_list):
+    _output = []
+    for y in range(len(_list)-1):
+        for x in range(y,len(_list)-1):
+            _output.append((_list[y], _list[x+1]))
+    return _output
+
+
+def combine(_list):
+    for linker in lijst[:-1]
+        for rechter in lijst[1:]
+            print linker, rechter
+
+
+for n, linker in enumerate(lijst[:-1]):
+    for rechter in lijst[n+1:]:
+        print n, rechter
+
+def combine(_list):
+    _output = []
+    for n, linker in enumerate(lijst[:-1]):
+        for rechter in lijst[n+1:]:
+            _output.append((linker, rechter))
+    return _output
+
+
+sets = {}
+
+for article in blog:
+    sets[article] = set(article.tags)
+
+combinations = combine(sets.keys())
+
+for a, b in combinations:
+    tags_in_common = sets[a] & sets[b]
+    for tag_a, tag_b in combine(tags_in_common):
+        graph(add_connnection tag_a,tag_b)
+
+pygraphviz
+
+
+
+^ dit wordt de achtergrond
+
+
+-- lijnen ---> naar de comments ook………………
+-- related articles ----> quotes -----> images
+
+------------
+|      ||__|
+|      ||__|
+|      ||
+
+related
+
+dingen hangen?
+
+
+
+
+
+two tags get linked if they share an article
+(multiple links?)
+
+each tag has many articles
+each article has many tags
+
+
+compare each tag to each tag
+
+shiftlist(list):
+    lis
+    return list, 
+
+
+for tags in blog:
+    add node tag:
+    
+
+for tags in blog:
+    add node tag
+    
+
+for article in blog:
+   for tag in article:
+       for articles linked tag
+       
+
+Ik ben geen homo om te zeggen dat ik lekkerder ben dan jou toch?
