cbccb55070e414ad3f367ba18c11239076c89796
codingisacopingstrategy <eric@ericschrijver.nl>
2011-09-30 14:28:44 +0200

    I thought I had commited this in different things

--- themes/commentpress/style/js/drawing.js
+++ themes/commentpress/style/js/drawing.js
@@ -1,14 +1,14 @@
-jQuery(function () {
-    jQuery("body").prepend('<canvas id="drawing_surface" style="position:absolute;top:0;left:0;"></canvas>')
+$(function () {
+    $("body").prepend('<canvas id="drawing_surface" style="position:absolute;top:0;left:0;"></canvas>')
     var cnvs = document.getElementById("drawing_surface");
     var ctx = cnvs.getContext("2d");
     
     var resetCanvas = function() {
-        ctx.canvas.width = jQuery("body").width();
-        ctx.canvas.height = jQuery(document).height();
+        ctx.canvas.width = $("body").width();
+        ctx.canvas.height = $(document).height();
     }
     
-    jQuery(window).resize(function() {
+    $(window).resize(function() {
         resetCanvas();
     });
 })
