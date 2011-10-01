f1364325592935c05d52596ca24964cb648c806b
codingisacopingstrategy <eric@ericschrijver.nl>
2011-10-01 17:27:40 +0200

    Link to tags
    (and there is only 1 text at the moment)

--- themes/commentpress/style/templates/archive_sidebar.php
+++ themes/commentpress/style/templates/archive_sidebar.php
@@ -5,8 +5,9 @@
 <ul>
   <li><a href="/report"><img src="<?php bloginfo('template_directory'); ?>/style/images/report.png" style="width:48px;height:48px;"/></a></li>
   <li><a href="/sounds"><img src="<?php bloginfo('template_directory'); ?>/style/images/sounds.png" style="width:48px;height:48px;"/></a></li>
-  <li><a href="/texts"><img src="<?php bloginfo('template_directory'); ?>/style/images/texts.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/texts/about"><img src="<?php bloginfo('template_directory'); ?>/style/images/texts.png" style="width:48px;height:48px;"/></a></li>
   <li><a href="/photos"><img src="<?php bloginfo('template_directory'); ?>/style/images/photos.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/tags"><img src="<?php bloginfo('template_directory'); ?>/style/images/tags.png" style="width:48px;height:48px;"/></a></li>
 </ul>
 
 </div><!-- /archive_sidebar -->
