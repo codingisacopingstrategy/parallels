7c8407c32f22ad8e09f21889689750cbf9931720
codingisacopingstrategy <eric@ericschrijver.nl>
2011-11-01 00:59:26 +0100

    The main site nav in comment pages
    Will have to put it somewhere else though,
    as a seperate entity underneath

--- themes/commentpress/style/templates/comments_sidebar.php
+++ themes/commentpress/style/templates/comments_sidebar.php
@@ -2,7 +2,15 @@
 
 <div id="comments_sidebar">
 
-
+<div class="site-nav">
+<ul>
+  <li><a href="/report"><img src="<?php bloginfo('template_directory'); ?>/style/images/report.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/sounds"><img src="<?php bloginfo('template_directory'); ?>/style/images/sounds.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/texts/about"><img src="<?php bloginfo('template_directory'); ?>/style/images/texts.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/photos"><img src="<?php bloginfo('template_directory'); ?>/style/images/photos.png" style="width:48px;height:48px;"/></a></li>
+  <li><a href="/tags"><img src="<?php bloginfo('template_directory'); ?>/style/images/tags.png" style="width:48px;height:48px;"/></a></li>
+</ul>
+</div>
 
 <div class="sidebar_header">
 <?php
